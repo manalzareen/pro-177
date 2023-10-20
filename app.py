0f2ba4ecc566fab986bfb843c6ddd06104f59cdc
manalzareen <81857933+manalzareen@users.noreply.github.com>
2023-10-20 19:15:05 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+templates=[
+    {
+        "inputs": 5,
+        "category":"Sports",
+        "word":"Chess"
+    },
+    {
+        "inputs":6,
+        "category":"European Country Name",
+        "word":"France"
+    },
+]
+
+@app.route("/")
+def index()
+return  render_template("index.html")
+
+@app.route("/get-template")
+def get_template():
+    return jsonify({
+        "status":"success",
+        "word":random.choice(templates)
+    })
